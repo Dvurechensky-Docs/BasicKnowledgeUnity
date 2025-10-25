ee53c31b0c465fffa0e317380c2999d4b8809f36
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-10-25 06:51:48 +0300

    Auto-update headers via pre_commit.ps1 - 2025-10-25

--- UnityProject/Assets/Scripts/Lesson4_2_Scale.cs
+++ UnityProject/Assets/Scripts/Lesson4_2_Scale.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 24 октября 2025 06:51:46
- * Version: 1.0.67
+ * Last Updated: 25 октября 2025 06:51:47
+ * Version: 1.0.68
  */
 
 using UnityEngine;
