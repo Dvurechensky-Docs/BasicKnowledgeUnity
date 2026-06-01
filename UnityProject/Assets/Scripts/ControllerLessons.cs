9d951561312719874b925aa4c08c07d311ce9ec7
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-01 14:46:17 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-01

--- UnityProject/Assets/Scripts/ControllerLessons.cs
+++ UnityProject/Assets/Scripts/ControllerLessons.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 31 мая 2026 14:54:28
- * Version: 1.0.287
+ * Last Updated: 01 июня 2026 14:46:16
+ * Version: 1.0.288
  */
 
 using UnityEngine;
