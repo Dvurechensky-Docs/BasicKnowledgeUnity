2cce391e5d16e7435fc16ea092c1b6ca92ff7342
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-15 06:51:36 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-15

--- UnityProject/Assets/Scripts/Lesson_7.cs
+++ UnityProject/Assets/Scripts/Lesson_7.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 июня 2026 16:34:32
- * Version: 1.0.301
+ * Last Updated: 15 июня 2026 06:51:35
+ * Version: 1.0.302
  */
 
 using UnityEngine;
